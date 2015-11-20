c41dce448db11d2064c588b1ad249d0efda167f5
chrisbassi96 <chrisbassi96@gmail.com>
2015-11-20 12:16:32 +0000

    Initial index.php in takserMAN

--- taskerMAN/index.php
+++ taskerMAN/index.php
@@ -1,7 +1,31 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Chris
- * Date: 20/11/2015
- * Time: 11:43
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>taskerMAN</title>
+</head>
+<body>
+<header>
+    <h1>taskerMAN</h1>
+    <a href="taskindex.php"><h3>Manage tasks</h3></a>
+    <a href="memberindex.php"><h3>Manage members</h3></a>
+    <hr />
+</header>
+<main>
+    <h2>
+        Welcome
+    </h2>
+    <form id="loginForm" action="tasksindex.php" method="POST">
+        <fieldset>
+            <legend>Please log in</legend>
+            <label>Username</label>
+            <input type="text" id="loginFormUsername" />
+            <br />
+            <label>Password</label>
+            <input type="text" id="loginFormPassword" />
+            <br />
+            <input type="submit" id="loginFormSubmit" value="Login" />
+        </fieldset>
+    </form>
+</main>
+</body>
+</html>
