7ff07a28773453ddb5780c638a70df228bb0ae61
chrisbassi96 <chrisbassi96@gmail.com>
2016-01-25 11:46:12 +0000

    Added code previously generated by init.php

--- taskerMAN/index.php
+++ taskerMAN/index.php
@@ -11,5 +11,21 @@ require_once("components/init.php");
 loadInit("default", $conn);
 ?>
 
+<main>
+    <h2>Welcome</h2>
+    <form id='loginForm' action='tasks/main.php' method='POST'>
+        <fieldset>
+            <legend>Please log in</legend>
+            <label>Username</label>
+            <input type='text' id='loginFormUsername' />
+            <br />
+            <label>Password</label>
+            <input type='text' id='loginFormPassword' />
+            <br />
+            <input type='submit' id='loginFormSubmit' value='Login' />
+            </fieldset>
+    </form>
+</main>
+
 </body>
 </html>
